481be5d6899324050ed7093c16dd6be0ebcd80ac
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-07-12 06:27:48 -0500

    fix layout

--- single.php
+++ single.php
@@ -4,12 +4,12 @@
     <div class="row mt-5 pt-4">
       <?php if (have_posts()) : ?>
         <?php while (have_posts()) : the_post(); ?>
-          <div class="col">
+          <div class="col-12">
             <span><?php the_category(' | ') ?></span>
             <h1 class="title"><?php the_title() ?></h1>
           </div>
           <div class="row mt-2">
-            <div class="col">
+            <div class="col-12">
               <?php the_post_thumbnail('large', ['class' => 'd-block mx-auto img-fluid']) ?>
             </div>
           </div>
