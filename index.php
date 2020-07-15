89cecd484ea1e412d14fd750e47ac57190e1e3f3
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-07-15 07:44:39 -0500

    add our-sections module

--- index.php
+++ index.php
@@ -12,15 +12,25 @@ $sliderArray = new WP_Query(
   )
 );
 
+$lastPostsArray = new WP_Query(
+  array(
+    'post_status'    => 'publish',
+    'posts_per_page' => 3,
+    'order'          =>'DESC',
+    'post__not_in'   => $sticky,
+    'orderby'        =>'ID'
+  )
+);
+
 $first = true; ?>
 
 <div class="row">
   <div class="col">
-    <?php if(isset($sticky[0])): ?>
+    <?php if ( isset($sticky[0]) ): ?>
     <div id="main-carousel" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
-        <?php for($cont == 0; $cont < sizeof($sliderArray->posts); $cont++): ?>
-        <li data-target="#main-carousel" data-slide-to="<?php echo $cont ?>" <?php if($cont == 0) echo 'class="active"' ?>></li>
+        <?php for ( $cont == 0; $cont < sizeof($sliderArray->posts); $cont++ ): ?>
+        <li data-target="#main-carousel" data-slide-to="<?php echo $cont ?>" <?php if ( $cont == 0 ) echo 'class="active"' ?>></li>
         <?php endfor ?>
       </ol>
       <div class="carousel-inner">
@@ -50,6 +60,19 @@ $first = true; ?>
   <div class="row mt-5">
     <div class="col-md-8">
       <h4 class="subtitle mb-3">EN NUESTRAS SECCIONES</h4>
+      <div class="card-columns" id="our-sections">
+        <?php $first = true;
+        while ( $lastPostsArray->have_posts() ) : $lastPostsArray->the_post(); $do_not_duplicate[] = get_the_ID(); ?>
+          <div class="card">
+            <?php the_post_thumbnail('large', ['class' => 'card-img-top']) ?>
+            <div class="card-body">
+              <p><?php the_category(' | ') ?></p>
+              <h5 class="card-title"><?php the_title() ?></h5>
+            </div>
+          </div>
+          <?php $first = false;
+        endwhile ?>
+      </div>
     </div>
     <div class="col-md-4">
       <h4 class="subtitle mb-3">LAS MÁS LEIDAS HOY</h4>
