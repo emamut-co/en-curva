f00f6b9c4a4467d3268f36f2fa1c0e156ad04dfb
Faber Andrés Vergara Holguín <fabervergara@gmail.com>
2020-07-18 15:11:45 -0500

    add content to single.php

--- single.php
+++ single.php
@@ -7,12 +7,19 @@
             <div class="col-12">
               <span class="text-dark"><?php the_category(' | ') ?></span>
               <h1 class="title"><?php the_title() ?></h1>
+              <span class="text-muted"><?php echo get_the_date( 'l, j M Y' ); ?></span>
             </div>
           </div>
           <div class="row mt-2">
             <div class="col-12">
               <?php the_post_thumbnail('large', ['class' => 'd-block mx-auto img-fluid']) ?>
             </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-md-8">
+              <?php the_content() ?>
+            </div>
+            <div class="col-md-4"></div>
           </div>
         <?php endwhile ?>
       <?php endif ?>
